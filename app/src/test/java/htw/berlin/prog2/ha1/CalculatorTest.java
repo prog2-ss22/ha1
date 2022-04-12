70ab507b3f41562f9df2bcbf3431c6408cfe4a87
Zeina Taher <Zeina.Taher@Student.HTW-Berlin.de>
2022-04-12 16:14:16 +0200

    Test commit

--- app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@DisplayName("Retro calculator")
+@DisplayName("1 Retro calculator")
 class CalculatorTest {
 
     @Test
