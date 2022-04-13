b16b386260c447045331827fa9b9532bf1fe95c8
CavdarciEnis <Enis.Cavdarci@Student.HTW-Berlin.de>
2022-04-13 15:00:37 +0200

    Neuer Grüner Test

--- app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -41,5 +41,23 @@ class CalculatorTest {
     }
 
     //TODO hier weitere Tests erstellen
+    @Test
+    @DisplayName("should display result after adding two positive multi-digit numbers")
+    void testDivision() {
+        Calculator calc = new Calculator();
+
+        calc.pressDigitKey(2);
+        
+        calc.pressBinaryOperationKey("/");
+        calc.pressDigitKey(2);
+        
+        calc.pressEqualsKey();
+
+        String expected = "1";
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+
+    }
 }
 
