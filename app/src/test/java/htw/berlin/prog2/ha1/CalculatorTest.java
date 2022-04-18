f5d2f18b45b7bb3cbb87340cdbfd71788fcca3e2
KruegelSimone <101262127+KruegelSimone@users.noreply.github.com>
2022-04-18 15:59:13 +0100

    Removed additional import in test

--- app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -5,8 +5,6 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.security.Key;
-
 @DisplayName("Retro calculator")
 class CalculatorTest {
 
