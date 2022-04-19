f6b895071d5f12887eea66fc72b229f81dc4eb2c
AysuBalcioglu <Aysu.Balcioglu@HTW-Berlin.de>
2022-04-20 01:55:29 +0200

    Teilaufgabe 1

--- app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -40,6 +40,23 @@ class CalculatorTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    @DisplayName("")
+
+    void testMult(){
+        Calculator calc = new Calculator();
+        calc.pressDigitKey(2);
+        calc.pressBinaryOperationKey("x");
+        calc.pressDigitKey(2);
+        calc.pressEqualsKey();
+
+        String expected ="4";
+        String actual = calc.readScreen();
+
+        assertEquals(expected,actual);
+
+    }
+
     //TODO hier weitere Tests erstellen
 }
 
