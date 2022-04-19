0fd0eb2e6cdb5af9902312aa0341761ec407b331
Christelle <tasiochristelle@yahoo.com>
2022-04-19 19:05:15 +0200

    1. Commit für neuer grüner Test

--- app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -1,18 +1,28 @@
 package htw.berlin.prog2.ha1;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DisplayName("Retro calculator")
 class CalculatorTest {
+    private Calculator calc;
+
+    @BeforeEach
+    void setUp() {
+        calc = new Calculator();
+    }
+
+    //-----------------------------------------------------------------------------------------
+    // Addition
+    //-----------------------------------------------------------------------------------------
 
     @Test
     @DisplayName("should display result after adding two positive multi-digit numbers")
     void testPositiveAddition() {
-        Calculator calc = new Calculator();
-
         calc.pressDigitKey(2);
         calc.pressDigitKey(0);
         calc.pressBinaryOperationKey("+");
@@ -27,9 +37,81 @@ class CalculatorTest {
     }
 
     @Test
+    @DisplayName("should display result after adding one negative multi-digit to one positive multi-digit number")
+    void testNegativeAdditionAlternative1() {
+        calc.pressDigitKey(2);
+        calc.pressDigitKey(0);
+        calc.pressNegativeKey();            // -20
+        calc.pressBinaryOperationKey("+");  // +
+        calc.pressDigitKey(2);              // 20
+        calc.pressDigitKey(0);
+        calc.pressEqualsKey();              // = 0
+
+        String expected = "0";
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("should display result after adding two negative multi-digit numbers")
+    void testNegativeAdditionAlternative2() {
+
+        calc.pressDigitKey(2);
+        calc.pressDigitKey(0);
+        calc.pressNegativeKey();            // -20
+        calc.pressBinaryOperationKey("+");  // +
+        calc.pressDigitKey(2);
+        calc.pressDigitKey(0);
+        calc.pressNegativeKey();            // -20
+        calc.pressEqualsKey();              // = -40
+
+        String expected = "-40";
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    @DisplayName("should display result after adding two negative multi-digit numbers")
+    void testFailureByAddition() {
+
+        calc.pressDigitKey(2);
+        calc.pressDigitKey(0);              // 20
+        calc.pressBinaryOperationKey("++"); // ++
+        calc.pressDigitKey(2);
+        calc.pressDigitKey(0);              // 20
+
+        assertThrows(IllegalArgumentException.class, () -> calc.pressEqualsKey());
+    }
+
+    //-----------------------------------------------------------------------------------------
+    // Subtraction
+    //-----------------------------------------------------------------------------------------
+
+    @Test
+    @DisplayName("should display result after subtracting two negative multi-digit numbers")
+    void testPositiveSubtraction() {
+
+        calc.pressDigitKey(2);
+        calc.pressDigitKey(0);              // 20
+        calc.pressBinaryOperationKey("-");  // -
+        calc.pressDigitKey(2);
+        calc.pressDigitKey(0);              // 20
+        calc.pressEqualsKey();              // = 0
+
+        String expected = "0";
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+    }
+
+    //-----------------------------------------------------------------------------------------
+    // Square Root
+    //-----------------------------------------------------------------------------------------
+
+    @Test
     @DisplayName("should display result after getting the square root of two")
     void testSquareRoot() {
-        Calculator calc = new Calculator();
 
         calc.pressDigitKey(2);
         calc.pressUnaryOperationKey("√");
