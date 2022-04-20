042d419544abf79fd2dfad0f6053f68246b0fed6
alshamou <mouaz.alshamaa@diconium.com>
2022-04-20 11:25:41 +0200

    Implementierung der neuen grünen Tests

--- app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -8,38 +8,100 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @DisplayName("Retro calculator")
 class CalculatorTest {
 
-    @Test
-    @DisplayName("should display result after adding two positive multi-digit numbers")
-    void testPositiveAddition() {
-        Calculator calc = new Calculator();
+  @Test
+  @DisplayName("should display result after adding two positive multi-digit numbers")
+  void testPositiveAddition() {
+    Calculator calc = new Calculator();
 
-        calc.pressDigitKey(2);
-        calc.pressDigitKey(0);
-        calc.pressBinaryOperationKey("+");
-        calc.pressDigitKey(2);
-        calc.pressDigitKey(0);
-        calc.pressEqualsKey();
+    calc.pressDigitKey(2);
+    calc.pressDigitKey(0);
+    calc.pressBinaryOperationKey("+");
+    calc.pressDigitKey(2);
+    calc.pressDigitKey(0);
+    calc.pressEqualsKey();
 
-        String expected = "40";
-        String actual = calc.readScreen();
+    String expected = "40";
+    String actual = calc.readScreen();
 
-        assertEquals(expected, actual);
-    }
+    assertEquals(expected, actual);
+  }
 
-    @Test
-    @DisplayName("should display result after getting the square root of two")
-    void testSquareRoot() {
-        Calculator calc = new Calculator();
+  @Test
+  @DisplayName("should display result after getting the square root of two")
+  void testSquareRoot() {
+    Calculator calc = new Calculator();
 
-        calc.pressDigitKey(2);
-        calc.pressUnaryOperationKey("√");
+    calc.pressDigitKey(2);
+    calc.pressUnaryOperationKey("√");
 
-        String expected = "1.41421356";
-        String actual = calc.readScreen();
+    String expected = "1.41421356";
+    String actual = calc.readScreen();
 
-        assertEquals(expected, actual);
-    }
+    assertEquals(expected, actual);
+  }
 
-    //TODO hier weitere Tests erstellen
+  @Test
+  @DisplayName("should display result after multiplying two positive multi-digit numbers")
+  void testPositiveMultiplication() {
+    Calculator calculator = new Calculator();
+    calculator.pressDigitKey(8);
+    calculator.pressDigitKey(1);
+    calculator.pressBinaryOperationKey("x");
+    calculator.pressDigitKey(2);
+    calculator.pressDigitKey(2);
+    calculator.pressEqualsKey();
+    String expected = "1782";
+    String actual = calculator.readScreen();
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  @DisplayName("should display result after multiplication of two negative numbers")
+  void testNegativeMultiplication() {
+    Calculator calculator = new Calculator();
+    calculator.pressDigitKey(8);
+    calculator.pressNegativeKey();
+    calculator.pressBinaryOperationKey("x");
+    calculator.pressDigitKey(7);
+    calculator.pressEqualsKey();
+    String expected = "-56";
+    String actual = calculator.readScreen();
+
+    assertEquals(expected, actual);
+
+  }
+
+  @Test
+  @DisplayName("should display result after division of a positive four digits number")
+  void testPositiveDivision() {
+    Calculator calculator = new Calculator();
+    calculator.pressDigitKey(9);
+    calculator.pressDigitKey(7);
+    calculator.pressDigitKey(5);
+    calculator.pressDigitKey(3);
+    calculator.pressBinaryOperationKey("/");
+    calculator.pressDigitKey(3);
+    calculator.pressEqualsKey();
+    String expected = "3251";
+    String actual = calculator.readScreen();
+
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  @DisplayName("should display result after addition of two decimal numbers")
+  void testAdditionDecimalNumbers() {
+    Calculator calculator = new Calculator();
+    calculator.pressDigitKey(1);
+    calculator.pressDotKey();
+    calculator.pressDigitKey(6);
+    calculator.pressBinaryOperationKey("+");
+    calculator.pressDigitKey(1);
+    calculator.pressDotKey();
+    calculator.pressDigitKey(7);
+    calculator.pressEqualsKey();
+    String expected = "3.3";
+    String actual = calculator.readScreen();
+    assertEquals(expected, actual);
+  }
 }
-
