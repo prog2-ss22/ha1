01059fe00ffaa16bef046f9811d786c05dbded68
KurtogluSinem <krtgl.sinem2519@gmail.com>
2022-04-20 15:38:18 +0200

    Erster Roter Test Bug fixed

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -123,9 +123,17 @@ public class Calculator {
             case "x" -> latestValue * Double.parseDouble(screen);
             case "/" -> latestValue / Double.parseDouble(screen);
             default -> throw new IllegalArgumentException();
+
         };
+        if (latestOperation == "/" && Double.parseDouble(screen) == 0){
+            screen = "ERROR";
+            return;
+        }
         screen = Double.toString(result);
         if(screen.endsWith(".0")) screen = screen.substring(0,screen.length()-2);
         if(screen.contains(".") && screen.length() > 11) screen = screen.substring(0, 10);
+
     }
 }
+
+
