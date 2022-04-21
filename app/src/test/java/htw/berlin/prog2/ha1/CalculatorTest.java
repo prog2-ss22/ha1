0d65537086afdcbfc5532a0f5426ee2339746bc8
lennartmanske <lennartmanske@gmail.com>
2022-04-21 15:31:05 +0200

    Teilaufgabe 1

--- app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -41,5 +41,83 @@ class CalculatorTest {
     }
 
     //TODO hier weitere Tests erstellen
+
+    // 1.
+    @Test
+    @DisplayName("should display result after converting a single-digit number into a percentage rate")
+    void testPercentageRate() {
+        Calculator calc = new Calculator();
+
+        calc.pressDigitKey(2);
+        calc.pressUnaryOperationKey("%");
+
+
+        String expected = "0.02";
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+    }
+
+    // 1.
+    @Test
+    @DisplayName("should display result after subtracting two negative single-digit numbers")
+    void testNegativeSubtractionsecond() {
+        Calculator calc = new Calculator();
+
+
+        calc.pressDigitKey(2);
+        calc.pressNegativeKey();
+        calc.pressBinaryOperationKey("-");
+        calc.pressDigitKey(2);
+        calc.pressEqualsKey();
+
+        String expected = "-4";
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+    }
+
+/*
+    // 2.
+    @Test
+    @DisplayName("should display Error after 1/0")
+    void testErrorOneDivisionZero() {
+        Calculator calc = new Calculator();
+
+        calc.pressDigitKey(1);
+        calc.pressBinaryOperationKey("/");
+        calc.pressDigitKey(0);
+        calc.pressEqualsKey();
+
+        String expected = "Error";
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+    }
+
+
+
+
+//2
+
+    @Test
+    @DisplayName("should display -2, using binary operation key in the beginning to have a negative first digit key")
+    void testNegativeError() {
+        Calculator calc = new Calculator();
+
+        calc.pressBinaryOperationKey("-");
+        calc.pressDigitKey(1);
+        calc.pressBinaryOperationKey("-");
+        calc.pressDigitKey(1);
+        calc.pressEqualsKey();
+
+        String expected = "-2";
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+    }
+*/
+
+
 }
 
