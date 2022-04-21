0c1d9792a4d1c2efa39d0931f1a39dbb2b74bf3b
minhthunguyen98 <bananashimshim@gmail.com>
2022-04-21 18:52:48 +0200

    1. Commit für Bugfix des esten roten Tests

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -125,7 +125,15 @@ public class Calculator {
             default -> throw new IllegalArgumentException();
         };
         screen = Double.toString(result);
-        if(screen.endsWith(".0")) screen = screen.substring(0,screen.length()-2);
-        if(screen.contains(".") && screen.length() > 11) screen = screen.substring(0, 10);
+
+        if(screen.endsWith(".0"))
+            screen = screen.substring(0,screen.length()-2);
+
+        if(screen.contains(".") && screen.length() > 11)
+            screen = screen.substring(0, 10);
+
+        if(latestOperation.equals("/") && screen.equals("Infinity")) {
+            screen = "Error";
+        }
     }
 }
