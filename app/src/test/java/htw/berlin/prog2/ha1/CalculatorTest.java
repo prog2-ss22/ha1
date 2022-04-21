aeec47c4031694612272a90961148c6099b85554
Zeina Taher <Zeina.Taher@Student.HTW-Berlin.de>
2022-04-21 22:57:46 +0200

    Commits, each commit using a comment

--- app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
+++ app/src/test/java/htw/berlin/prog2/ha1/CalculatorTest.java
@@ -41,5 +41,56 @@ class CalculatorTest {
     }
 
     //TODO hier weitere Tests erstellen
+
+    @Test
+    @DisplayName("should display result after multiplying two positive numbers")
+    void testProdukt() {
+        // Test Aufgabe 1
+        Calculator calc = new Calculator();
+        calc.pressDigitKey(0);
+        calc.pressDigitKey(3);
+        calc.pressBinaryOperationKey("x");
+        calc.pressDigitKey(0);
+        calc.pressDigitKey(3);
+        calc.pressEqualsKey();
+
+        String expected = "9";
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+    }
+
+   @Test
+   @DisplayName("should display result after dividing any number with zero")
+   void testDividieren() {
+        // Test Aufgabe 2
+       Calculator calc = new Calculator();
+       calc.pressDigitKey(1);
+       calc.pressDigitKey(0);
+       calc.pressBinaryOperationKey("/");
+       calc.pressDigitKey(0);
+       calc.pressEqualsKey();
+
+       String expected = "ERROR";
+       String actual = calc.readScreen();
+
+       assertEquals(expected, actual);
+   }
+
+    @Test
+    @DisplayName("should display result after getting the square root of seven")
+    void testRunden() {
+        Calculator calc = new Calculator();
+
+        calc.pressDigitKey(7);
+        calc.pressUnaryOperationKey("√");
+
+        String expected = "2.64575131106";
+        String actual = calc.readScreen();
+
+        assertEquals(expected, actual);
+    }
+
+
 }
 
