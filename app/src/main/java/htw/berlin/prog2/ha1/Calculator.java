6a875c492a15224473f5c6695e50b4817c8a11de
Benjamin <Benjamin.Masters@Student.HTW-Berlin.de>
2023-04-20 20:15:06 +0200

    Teilaufgabe 2: Testpercent grün

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -81,8 +81,12 @@ public class Calculator {
             default -> throw new IllegalArgumentException();
         };
         screen = Double.toString(result);
-        if(screen.contains(".") && screen.length() > 11) screen = screen.substring(0, 10);
-
+        if (screen.contains(".") && screen.length() > 11) {
+            screen = screen.substring(0, 10);
+        }
+        if (screen.endsWith(".0")) {
+            screen = screen.substring(0, screen.length()-2);
+        }
     }
 
     /**
